6523ae4e5bcfa62ce5e5393330416eef9eb8a92d
tizee <33030965+tizee@users.noreply.github.com>
2021-10-14 11:07:17 +0800

    feat: 748 div3

--- 748-div3/b.cpp
+++ 748-div3/b.cpp
@@ -1,106 +1,136 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <string>
 #include <vector>
 
+#include <cmath>
+#include <cstdio>
+
 using namespace std;
-using ll=long long;
 
-vector<int> digits(ll num){
-  vector<int> res;
-  while(num>10){
-    res.push_back(num%10);
-    num/=10;
-  }
-  if(num>0){
-    res.push_back(num);
-  }
-  reverse(res.begin(),res.end());
-  return res;
-}
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
 
-int max(int a,int b){
-  return a>b?a:b;
-}
+#define endl "\n"
+#define min(x, y) ((x) > (y) ? (y) : (x))
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define itoc(i) (char)(i + '0')
+#define pb push_back
+#define pob pop_back
 
-int min(int a,int b){
-return a>b?b:a;
-}
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define ALL(a) a.begin(), a.end()
+#define FR(i, n) for (ll i = 0; i < n; i++)
+#define FOR(i, s, e) for (ll i = s; i < e; i++)
+#define RFR(i, n) for (ll i = n - 1; i >= 0; i--)
+#define RFOR(i, s, e) for (ll i = s; i >= e; i--)
 
-ll get_val(vector<int>&nums,vector<bool> &removed){
-  ll res=0;
-  int end = nums.size();
-  int count=0;
-  for(ll i = 0;i<nums.size();i++){
-    if(!removed[i]){
-      count++;
+#define DBall(a)                                                               \
+  cerr << #a << " => ";                                                        \
+  for (auto &i : a) {                                                          \
+    cerr << i << " ";                                                          \
+  }                                                                            \
+  cerr << endl;
+
+#define DBrange(a, k, n)                                                       \
+  cerr << #a << " => ";                                                        \
+  for (int i = k; i < n; i++) {                                                \
+    cerr << a[i] << " ";                                                       \
+  }                                                                            \
+  cerr << endl;
+
+const ll inf = 1e9, MOD = 1e9 + 7;
+
+class Solution {
+private:
+public:
+    // WRITE YOUR SOLUTION HERE
+    void
+    solve() {
+        string s;
+        cin >> s;
+        reverse(s.begin(), s.end());
+        // 50 75 25 100
+        cout << find25s(s) << endl;
     }
-  }
-  for(int i = nums.size()-1;i>0;i--){
-    if(nums[i]!=0&&!removed[i]){
-      break;
-    }
-    if(nums[i]!=0&&nums[i-1]==0&&removed[i]){
-      end= i;
-    }
-  }
 
-  while(end<nums.size()&&end>0&&nums[end-1]==0){
-    end--;
-  }
+    int
+    find25s(string &s) {
+        // 05 00 57 52
+        int steps = INT_MAX;
+        int first0 = 0, first5 = 0;
+        int n = s.size();
+        // state
+        // 0 -> 5 or 0 -> 0
+        int i = 0, state = 0;
 
-  for(int i = 0;i<end;i++){
-    if(!removed[i]){
-      res = res*(ll)10+(ll)nums[i];
-      count--;
+        while (i < n) {
+            if (state==0&&s[i] == '0') {
+                state = 1;
+                first0=i;
+                i++;
+            } else if (state == 0) {
+                i++;
+            } else if (state == 1 && (s[i] == '5' || s[i] == '0')) {
+                // no leading zero
+                steps = min(i - first0 - 1 + first0, steps);
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        // 5 -> 7 or 5 -> 2
+        i=0,state=0;
+        while (i < n) {
+            if (state==0&&s[i] == '5') {
+                state = 1;
+                first5=i;
+                i++;
+            } else if (state == 0) {
+                i++;
+            } else if (state == 1 && (s[i] == '7' || s[i] == '2')) {
+                // no leading zero
+                steps = min(i - first5 - 1 + first5, steps);
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        return steps;
     }
-  }
-  if(count>0){
-    res*=10;
-  }
-  return res;
+};
+
+/* #define OJ_TEST */
+
+int
+main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+#ifndef OJ_TEST
+    freopen("testb/1", "r", stdin);
+    freopen("testb/1.test.out", "w+", stdout);
+#endif
+
+    FASTIO;
+    Solution ans;
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        ans.solve();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    cerr << "Time spent: " << elapsed.count() * 1e-9 << "seconds.\n";
+    return 0;
 }
-
-void backtracking(int &steps,vector<int> &nums, vector<bool> &removed, int count,int cur_idx){
-  if(count<=2 || cur_idx == nums.size()){
-    return;
-  }
-  // 25 50 75 100
-  for(ll i = cur_idx;i<nums.size();i++){
-    if(!removed[i]){
-      removed[i]=true;
-      ll val = get_val(nums,removed);
-      if(val>0&&val%(ll)25==0){
-        cout<<"n:"<<nums.size()<<",count:"<<count<<",steps:"<<steps<<endl;
-        cout<<"val:"<<val<<endl;
-        steps = min(nums.size()-count+1,steps);
-        removed[i]=false;
-        return;
-      }
-      backtracking(steps, nums, removed, count-1,cur_idx+1);
-      removed[i]=false;
-    }
-  }
-
-}
-
-int main(int argc, char *argv[])
-{
-  int n;
-  ll num;
-  cin>>n;
-  while(n>0){
-    cin>>num;
-    // corner case
-    int steps=0;
-    if(num%(ll)25!=0){
-      steps=INT_MAX;
-      vector<int> nums=digits(num);
-      vector<bool> removed(nums.size(),false);
-      backtracking(steps,nums,removed,nums.size(),0);
-    }
-    cout<<steps<<endl;
-    n--;
-  }
-  return 0;
-}
-
